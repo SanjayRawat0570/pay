b5810e75c994ac28c1b6c4d1343b4a4fb14e9b8a
Sanjay Rawat <sanjayrawat0570@gmail.com>
2025-03-02 22:08:13 +0530

    initial commit

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
 
+mongoose.connect("mongodb+srv://sanjayrawat0570:7Sxaoj2LRCWDZyNO@cluster0.gxm0w.mongodb.net/pay")
+
+const userSchema = mongoose.Schema({
+    username : String,
+    password : string ,
+    firstname : String,
+    lastname : String,
+}) 
+
+const User = mongoose.model('User', userSchema);
+MediaSourceHandle.exports = {
+    User
+}
