f6560f3f2b60f55b7fc39f51a3f809d2f324828c
Sanjay Rawat <sanjayrawat0570@gmail.com>
2025-03-02 22:07:37 +0530

    initial

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,12 +1,1 @@
-const mongoose = require('mongoose');
-
-mongoose.connect("mongodb+srv://sanjayrawat0570:7Sxaoj2LRCWDZyNO@cluster0.gxm0w.mongodb.net/pay")
-
-const userSchema = mongoose.Schema({
-    username : String,
-    password : string ,
-    firstname : String,
-    lastname : String,
-}) 
-
 
